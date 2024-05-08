2f5149ba013a736b611834799d5dda6e9571aeab
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-05-08 13:55:45 -0500

    PROBLEMS VIEWING IN LOCALHOST

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ 
+    helpers: require('./utils/helpers') });
+
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -26,13 +29,12 @@ const sess = {
 };
 
 app.use(session(sess));
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 
 
@@ -42,8 +44,3 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
-
-
-// app.use(express.json());
-
-
